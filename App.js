afa284a731d3c10dcbb9fb99a2a8307ae90d5184
ModestasV <info@modestasv.lt>
2020-12-02 15:15:45 +0200

    React native example with Chuck Norris jokes

--- App.js
+++ App.js
@@ -1,21 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar'
+import React from 'react'
+import {StyleSheet, Text, View, FlatList, Image} from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_URL = 'https://api.chucknorris.io/jokes/search?query=cats'
+const COLOR = '#0015b0'
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      jokesList: []
+    }
+  }
+
+  componentDidMount () {
+    this.apiCall()
+  }
+
+  async apiCall () {
+    let resp = await fetch(API_URL)
+    let json = await resp.json()
+    this.setState({jokesList: json.result})
+  }
+
+
+  render () {
+    return (
+      <View style={styles.container}>
+
+        <View style={styles.header}>
+          <Text style={styles.headerText}>React Native example app</Text>
+        </View>
+
+        <FlatList
+          ItemSeparatorComponent={() => <View style={styles.separator}/>}
+          data={this.state.jokesList}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => (
+            <View style={styles.item}>
+              <View style={{width: '15%'}}>
+                <Image
+                  style={styles.tinyIcon}
+                  source={{uri: item.icon_url}}
+                />
+              </View>
+              <View style={{width: '85%'}}>
+                <Text style={{fontWeight: 'bold'}}>{item.created_at}</Text>
+                <Text>{item.value}</Text>
+              </View>
+            </View>
+          )}
+        />
+      </View>
+    )
+  }
 }
 
+export default App
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  statusBar: {
+    backgroundColor: COLOR
+  },
+  header: {
+    backgroundColor: COLOR,
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
+    height: 50
   },
-});
+  headerText: {
+    color: '#FFF'
+  },
+  container: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#CED0CE'
+  },
+  item: {
+    padding: 10,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start'
+  },
+  tinyIcon: {
+    width: 50,
+    height: 50,
+  }
+})
